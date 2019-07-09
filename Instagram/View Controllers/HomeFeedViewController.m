0785948c21385b076c1d24e4702741fb3b80acfa
Mario Baxter <mariobaxter@fb.com>
2019-07-09 12:59:59 -0700

    Added a custom cell called PostCell and gave it a reuse identifier of PostCell.

--- Instagram/View Controllers/HomeFeedViewController.m
+++ Instagram/View Controllers/HomeFeedViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "HomeFeedViewController.h"
+#import "PostCell.h"
 
-@interface HomeFeedViewController ()
+@interface HomeFeedViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,6 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 
     // Set the title of the nav item to be the Instagram icon and put it in the middle
     UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"1200px-Instagram_logo.svg"]];
@@ -36,5 +41,14 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    PostCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"PostCell"];
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 100;
+}
 
 @end
