9b432bc46339e7317832b7e8992895572ad4373c
Mario Baxter <mariobaxter@fb.com>
2019-07-09 12:11:33 -0700

    Added Instagram logo to the top of all the view controllers embedded in the tab bar controller

---
+++ Instagram/View Controllers/NewPostViewController.h
@@ -0,0 +1,17 @@
+//
+//  NewPostViewController.h
+//  Instagram
+//
+//  Created by mariobaxter on 7/9/19.
+//  Copyright © 2019 mariobaxter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NewPostViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/View Controllers/NewPostViewController.m
@@ -0,0 +1,40 @@
+//
+//  NewPostViewController.m
+//  Instagram
+//
+//  Created by mariobaxter on 7/9/19.
+//  Copyright © 2019 mariobaxter. All rights reserved.
+//
+
+#import "NewPostViewController.h"
+
+@interface NewPostViewController ()
+
+@end
+
+@implementation NewPostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    // Set the title of the nav item to be the Instagram icon and put it in the middle
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"1200px-Instagram_logo.svg"]];
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    UIView *titleView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 120, 44)];
+    imageView.frame = titleView.bounds;
+    [titleView addSubview:imageView];
+    self.navigationItem.titleView = titleView;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
