2deb05b6ff1480440b336a018ff0f02001389d17
Mario Baxter <mariobaxter@fb.com>
2019-07-11 12:00:34 -0700

    Added infinite scrolling functionality although there is a small bug that causes the table view to jump up one item when reloading.

--- Instagram/View Controllers/HomeFeedViewController.m
+++ Instagram/View Controllers/HomeFeedViewController.m
@@ -15,11 +15,17 @@
 #import "LoginViewController.h"
 #import "UIImageView+AFNetworking.h"
 #import "DetailViewController.h"
+#import "InfiniteScrollActivityView.h"
 
-@interface HomeFeedViewController () <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@interface HomeFeedViewController () <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSMutableArray *posts;
 @property (strong, nonatomic) UIRefreshControl *refreshControl;
+@property (assign, nonatomic) BOOL isMoreDataLoading;
+@property (strong, nonatomic) InfiniteScrollActivityView* loadingMoreView;
+@property (nonatomic) BOOL hasLoadedEverything;
+
 @end
 
 @implementation HomeFeedViewController
@@ -35,6 +41,8 @@
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(updateTable) forControlEvents:UIControlEventValueChanged];
     [self.tableView insertSubview:self.refreshControl atIndex:0];
+    
+    self.hasLoadedEverything = NO;
 
     // Set the title of the nav item to be the Instagram icon and put it in the middle
     UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"1200px-Instagram_logo.svg"]];
@@ -43,6 +51,16 @@
     imageView.frame = titleView.bounds;
     [titleView addSubview:imageView];
     self.navigationItem.titleView = titleView;
+    
+    // Set up Infinite Scroll loading indicator
+    // (startX, startY, widht, height)
+    CGRect frame = CGRectMake(0, self.tableView.contentSize.height, self.tableView.bounds.size.width, InfiniteScrollActivityView.defaultHeight);
+    self.loadingMoreView = [[InfiniteScrollActivityView alloc] initWithFrame:frame];
+    self.loadingMoreView.hidden = true;
+    [self.tableView addSubview:self.loadingMoreView];
+    UIEdgeInsets insets = self.tableView.contentInset;
+    insets.bottom += InfiniteScrollActivityView.defaultHeight;
+    self.tableView.contentInset = insets;
     
     [self updateTable];
 }
@@ -57,13 +75,12 @@
     [query includeKey:@"username"];
     
     //[query whereKey:@"likesCount" greaterThan:@100];
-    query.limit = 20;
+    [query setLimit:20];
     // fetch data asynchronously
     [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
         if (posts != nil) {
             // do something with the array of objects returned by the call
             self.posts = [NSMutableArray arrayWithArray:posts];
-            NSLog(@"%@", self.posts);
             [self.tableView reloadData];
             [self.refreshControl endRefreshing];
         } else {
@@ -98,7 +115,7 @@
 }
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    PostCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"PostCell"];
+    PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell" forIndexPath:indexPath];
     Post *post = self.posts[indexPath.row];
     NSURL *imageURL = [NSURL URLWithString:post.image.url];
     [cell.image setImageWithURL:imageURL];
@@ -113,6 +130,51 @@
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.posts.count;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    // Handle scroll behavior here
+    if(!self.isMoreDataLoading){
+        // Calculate the position of one screen length before the bottom of the results
+        int scrollViewContentHeight = self.tableView.contentSize.height;
+        int scrollOffsetThreshold = scrollViewContentHeight - self.tableView.bounds.size.height;
+        
+        // When the user has scrolled past the threshold, start requesting
+        if(scrollView.contentOffset.y > scrollOffsetThreshold && self.tableView.isDragging) {
+            self.isMoreDataLoading = true;
+            
+            CGRect frame = CGRectMake(0, self.tableView.contentSize.height, self.tableView.bounds.size.width, InfiniteScrollActivityView.defaultHeight);
+            self.loadingMoreView.frame = frame;
+            [self.loadingMoreView startAnimating];
+            
+            PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+            [query orderByDescending:@"createdAt"];
+            [query setSkip:self.posts.count];
+            [query includeKey:@"author"];
+            [query includeKey:@"createdAt"];
+            [query includeKey:@"username"];
+            [query setLimit:20];
+            
+            [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+                if (!self.hasLoadedEverything) {
+                    if (posts != nil) {
+                        if (posts.count < 2) {
+                            self.hasLoadedEverything = YES;
+                        }
+                        // do something with the array of objects returned by the call
+                        [self.posts addObjectsFromArray:posts];
+                        NSLog(@"%u", (unsigned) self.posts.count);
+                        [self.tableView reloadData];
+                        self.isMoreDataLoading = false;
+                        [self.loadingMoreView stopAnimating];
+                    } else {
+                        NSLog(@"%@", error.localizedDescription);
+                    }
+                }
+            }];
+            [self.loadingMoreView stopAnimating];
+        }
+    }
 }
 
 @end
